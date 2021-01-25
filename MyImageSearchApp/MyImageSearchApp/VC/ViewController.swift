9d83166b26843f99a5344e0b0e9c5ef818e136d4
김동현 <gimdonghyeon@gimdonghyeon-ui-MacBookPro.local>
2021-01-25 22:47:37 +0900

    Created New Project

---
+++ MyImageSearchApp/MyImageSearchApp/VC/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  MyImageSearchApp
+//
+//  Created by 김동현 on 2021/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        configureNavi()
+        configureViews()
+    }
+    
+    // MARK: - Actions
+    
+    
+    // MARK: - Helpers
+    
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        
+    }
+    
+    // MARK: - ConfigureNavi
+    
+    private func configureNavi() {
+        
+    }
+    
+    // MARK: - ConfigureViews
+    
+    private func configureViews() {
+        view.backgroundColor = .red
+    }
+
+
+}
+
